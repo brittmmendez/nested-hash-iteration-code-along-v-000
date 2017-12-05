a73e2f8cd34df47807ae279f06fedfa691c4b7e9
Brittany Morris <brittmmendez@gmail.com>
2017-12-05 19:50:47 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,14 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person,data|
+    data.each do |attribute,value|
+      if attribute==:favorite_ice_cream_flavors
+        value.delete_if do |ice_cream|
+          ice_cream=="strawberry"
+        end
+      end
+    end
+  end
 end
+
